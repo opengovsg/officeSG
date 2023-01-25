ed7d7d8691153fbe308cab3233a6eede08230f8e
datle <thanhdat.toby@gmail.com>
2023-01-25 17:34:51 +0800

    feat: allow all cors origin

--- play/src/pusher/middlewares/Cors.ts
+++ play/src/pusher/middlewares/Cors.ts
@@ -2,13 +2,16 @@ import type { MiddlewareNext, MiddlewarePromise, Request, Response } from "hyper
 import { ALLOWED_CORS_ORIGIN } from "../enums/EnvironmentVariable";
 
 export function cors(req: Request, res: Response, next: MiddlewareNext): MiddlewarePromise | void {
+    console.log("cors");
     if (ALLOWED_CORS_ORIGIN) {
+        console.log("cors allowed");
         res.setHeader(
             "access-control-allow-headers",
             "Origin, X-Requested-With, Content-Type, Accept, Authorization, Pragma, Cache-Control"
         );
         res.setHeader("access-control-allow-methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         if (ALLOWED_CORS_ORIGIN === "*") {
+            console.log("cors set to *");
             res.setHeader("access-control-allow-origin", req.header("Origin"));
         } else {
             res.setHeader("access-control-allow-origin", ALLOWED_CORS_ORIGIN);
